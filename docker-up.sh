f5236050d66f22b9875c3f3fb7a3f5a4994dfa92
Matt Howlett <matt@mhowlett.com>
2016-06-12 22:52:44 -0700

    sdf

--- docker-up.sh
+++ docker-up.sh
@@ -17,6 +17,6 @@ docker run -d --name kafka \
     confluent/kafka
 
 docker run -d --name producer \
-    java:8u91 \
     -v /git/target:/target \
+    java:8u91 \
     java -jar /target/kafka-low-end-test-1.0-SNAPSHOT-jar-with-dependencies.jar 1000
