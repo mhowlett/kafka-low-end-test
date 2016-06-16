79576b65b36fbb63316f246511430ff00e83b5fb
Matt Howlett <matt@mhowlett.com>
2016-06-15 20:13:24 -0700

    wR

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ cd /opt
 wget -O jre32.tar.gz http://javadl.oracle.com/webapps/download/AutoDL?BundleId=207763
 tar xvf jre32.tar.gz
 
+update-alternatives --install /usr/bin/java java /opt/jre1.8.0_91/bin/java 100
+
 wget http://packages.confluent.io/archive/3.0/confluent-3.0.0-2.11.tar.gz
 tar xvf confluent-3.0.0-2.11.tar.gz
-
