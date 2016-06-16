b2dbbfe324799ffea9ca6ff17bc65d0e9bcf0f75
Matt Howlett <matt@mhowlett.com>
2016-06-15 19:59:49 -0700

    Qwe

--- setup.sh
+++ setup.sh
@@ -3,10 +3,9 @@
 mkdir /opt
 cd /opt
 
-wget http://packages.confluent.io/archive/3.0/confluent-3.0.0-2.11.tar.gz
-tar xvf confluent-3.0.0-2.11.tar.gz
-
 wget -O jre32.tar.gz http://javadl.oracle.com/webapps/download/AutoDL?BundleId=207763
 tar xvf jre32.tar.gz
 
+wget http://packages.confluent.io/archive/3.0/confluent-3.0.0-2.11.tar.gz
+tar xvf confluent-3.0.0-2.11.tar.gz
 
