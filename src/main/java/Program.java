3edd3b5ced194311d4e340e53104a14a8cc6b8b0
Matt Howlett <matt@mhowlett.com>
2016-06-20 20:45:07 -0700

    wr

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -57,6 +57,7 @@ public class Program {
     props.put("enable.auto.commit", "true");
     props.put("auto.commit.interval.ms", "1000");
     props.put("session.timeout.ms", "30000");
+    props.put("auto.offset.reset", "smallest");
     props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
     props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
     KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
