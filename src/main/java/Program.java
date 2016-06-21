bdc9710cf13cb59a7ddc3ff109356da58fe8fc7c
Matt Howlett <matt@mhowlett.com>
2016-06-20 20:34:26 -0700

    wr

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -55,7 +55,8 @@ public class Program {
     Properties props = new Properties();
     props.put("bootstrap.servers", "localhost:9092");
     props.put("group.id", "test");
-    props.put("enable.auto.commit", "false");
+    props.put("enable.auto.commit", "true");
+    props.put("auto.commit.interval.ms", "1000");
     props.put("session.timeout.ms", "30000");
     props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
     props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
@@ -66,7 +67,7 @@ public class Program {
       for (ConsumerRecord<String, String> record : records) {
         System.out.printf("offset = %d, key = %s, value = %s", record.offset(), record.key(), record.value());
       }
-      consumer.commitSync();
+      // consumer.commitSync();
     }
   }
 
