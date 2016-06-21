6e58e6dc8195ef6410b97c2975663f34e256fcd2
Matt Howlett <matt@mhowlett.com>
2016-06-20 20:31:10 -0700

    wr

--- kafka.sh
+++ kafka.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-KAFKA_HEAP_OPTS="-Xmx6M -Xms6M" /opt/confluent-3.0.0/bin/kafka-server-start ./config/server.properties
+KAFKA_HEAP_OPTS="-Xmx16M -Xms16M" /opt/confluent-3.0.0/bin/kafka-server-start ./config/server.properties
