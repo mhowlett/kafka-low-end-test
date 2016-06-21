66780d41f4fdf8545272f1821d16179186e2f2c7
Matt Howlett <matt@mhowlett.com>
2016-06-20 18:57:09 -0700

    updates to config

--- kafka.sh
+++ kafka.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-KAFKA_HEAP_OPTS="-Xmx64M -Xms64M" /opt/confluent-3.0.0/bin/kafka-server-start ./config/server.properties
+KAFKA_HEAP_OPTS="-Xmx32M -Xms32M" /opt/confluent-3.0.0/bin/kafka-server-start ./config/server.properties
